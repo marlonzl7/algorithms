9dc7a59c910a581a391d5581c6ee60ee665ec63c
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-07-18 19:43:41 -0300

    feat(c/sort): adicionar biblioteca util com funções auxiliares para algoritmos de ordenação

---
+++ c/lib/util.h
@@ -0,0 +1,14 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+void initRandom(void);
+
+int fillArray(int *arr, int length);
+
+void printArray(int *arr, int length);
+
+int fillMatrix(int *arr, int rows, int cols);
+
+void printMatrix(int *arr, int rows, int cols);
+
+#endif
