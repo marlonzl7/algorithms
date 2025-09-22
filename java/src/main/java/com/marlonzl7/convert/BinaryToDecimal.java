74592cec1c7b3a2679bc5bad62676f8962d48d0d
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-09-22 00:32:58 -0300

    fix(java/convert): corrigir algoritmo de conversão BinaryToDecimal

--- java/src/main/java/com/marlonzl7/convert/BinaryToDecimal.java
+++ java/src/main/java/com/marlonzl7/convert/BinaryToDecimal.java
@@ -2,7 +2,7 @@ package com.marlonzl7.convert;
 
 public class BinaryToDecimal {
     public static void main(String[] args) {
-        int[] binary = {0, 1, 1, 1};
+        int[] binary = {1, 0, 0, 1};
 
         System.out.print("Binário: ");
         for (int i = 0; i < binary.length; i++) {
@@ -16,8 +16,9 @@ public class BinaryToDecimal {
     public static int convertBinaryToDecimal(int[] binary) {
         int decimal = 0;
 
-        for (int i = 0; i < binary.length; i++) {
-            decimal = decimal * 2 + binary[i];
+        for (int i = binary.length - 1; i >= 0; i--) {
+            int j = binary.length - i - 1;
+            decimal += (binary[i] == 1) ? (int) Math.pow(2, j) : 0;
         }
 
         return decimal;
